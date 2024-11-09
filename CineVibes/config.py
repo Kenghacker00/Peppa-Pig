6c0ab94ac7cdb566789234376bbf5c08bbc3f5ca
kengUNI <163786810+Kenghacker00@users.noreply.github.com>
2024-11-08 20:33:22 -0600

    Delete CineVibes directory

--- CineVibes/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'tu_clave_secreta'
-    DATABASE = 'database/cinevibes.db'
-    EMAIL_SENDER = 'vibescine10@gmail.com'  # Cambia esto por tu email
-    EMAIL_PASSWORD = 'adoi iehs jvmm itme'  # Cambia esto por tu contraseña
-    IMDB_API_KEY = 'http://www.omdbapi.com/?i=tt3896198&apikey=3e192711'  # Cambia esto por tu API Key de IMDb
